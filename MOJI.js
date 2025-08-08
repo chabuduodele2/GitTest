ffc074a8dbad3b13b5dc66d67479b3b519730ade
Yongkang Peng <p54710242@gmail.com>
2025-08-08 19:40:53 +0800

    Update MOJI.js

--- MOJI.js
+++ MOJI.js
@@ -8,26 +8,24 @@
 hostname = api.mojidict.com
 
 
-let obj = JSON.parse($response.body);
+let Premium = $response.body;
+var modified = JSON.parse(Premium);
 
-// 替换会员状态字段
 try {
-  const target = obj.result.results["fetch-XTest"].result["8TAAYC1tSZ"];
-
-  target.hasPaid = true;
-  target.isOpen = true;
-  target.privilege = {
+  modified.result.results["fetch-XTest"].result["8TAAYC1tSZ"].hasPaid = true;
+  modified.result.results["fetch-XTest"].result["8TAAYC1tSZ"].isOpen = true;
+  modified.result.results["fetch-XTest"].result["8TAAYC1tSZ"].privilege = {
     "identity": "000-002-00001",
     "privilege": {
       "totalInServiceDays": 9999,
-      "expiresDate": 4070880000000,  // 2098年
-      "purchaseDate": 1672502400000  // 2023年
+      "expiresDate": 4070880000000,
+      "purchaseDate": 1672502400000
     },
     "privilegeStatus": "activated",
     "canPay": true
   };
 } catch (e) {
-  console.log("修改失败：", e);
+  console.log("处理失败：", e);
 }
 
-$done({ body: JSON.stringify(obj) });
+$done({ body: JSON.stringify(modified) });
